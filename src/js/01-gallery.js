5bad04be6666d34e38708dff03cf0c4856560141
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-03-08 22:36:08 +0200

    fin 1 and 2 homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const addGalleryItems = galleryItems.map(({ preview, original, description }) => `<a class="gallery__item gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`).join("");
+
+galleryEl.insertAdjacentHTML('beforeend', addGalleryItems);
+
+let gallery = new SimpleLightbox('.gallery__item', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
+
+const partOfMarking = marking(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", partOfMarking);
+let lightbox = new SimpleLightbox(".gallery a", {
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
 
+const feedBackForm = document.querySelector('.feedback-form');
+
+function hendlerSaveData({ target }) {
+    const KEY = "feedback-form-state";
+    localStorage.setItem(KEY, JSON.stringify({email, message}))
+    console.log();
+    if (target.type === 'email') {
+        console.dir(localStorage);
+
+        console.log(localStorage.getItem(KEY));
+    }
+    if (target.type === 'textarea') {
+        localStorage.setItem(KEY, JSON.stringify({message: target.value.trim()}))
+        console.log(target.type);
+    }
+
+};
+
+feedBackForm.addEventListener('input', hendlerSaveData)
