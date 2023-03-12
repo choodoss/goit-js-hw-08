62dc43ba98cd55faf890cc5869908e41d8f708f4
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2023-03-12 10:30:41 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,50 +1,35 @@
 import throttle from 'lodash.throttle';
 
 const feedBackForm = document.querySelector('.feedback-form');
-const emailInputEl = feedBackForm.querySelector('input');
-const messageInputEl = feedBackForm.querySelector('textarea');
-
 const KEY = "feedback-form-state";
-resetSaveData();
-const data = {
-    email: '',
-    message: '',
+resetForm();
+
+function hendlerInputForm({ target }) {
+    let localData = localStorage.getItem(KEY);
+    localData = localData !== true ? JSON.parse(localData) : {};
+    console.log(localStorage.getItem(KEY))
+    localData[target.name] = target.value;
+    localStorage.setItem(KEY, JSON.stringify(localData))
 };
 
-function hendlerSaveData({ target }) {
-    const localData = JSON.parse(localStorage.getItem(KEY));
-    if (localData !== null) {
-        data.email = localData.email;
-        data.message = localData.message;
-    }
-    if (target.type === 'email') {
-        data.email = target.value;
-    }
-    if (target.type === 'textarea') {
-        data.message = target.value
-    }
-    localStorage.setItem(KEY, JSON.stringify(data))
-
-};
-function hendlerSend(e) {
+function hendlerSavedata(e) {
     e.preventDefault();
-    const localData = JSON.parse(localStorage.getItem(KEY));
-    console.log(`email: ${localData.email}`)
-    console.log(`message: ${localData.message}`)
+    console.log(feedBackForm.elements)
+    for (let i = 0; i < 2; i += 1) {
+        console.log(`${feedBackForm.elements}: ${feedBackForm.elements[i].value}`)
+    }
+    console.log(`${feedBackForm.elements}: ${feedBackForm.elements[0].value}`)
 
-    e.currentTarget.reset();
-    localStorage.removeItem(KEY);
-    data.message = '';
-    data.email = '';
 };
 
-function resetSaveData() {
-    const localData = JSON.parse(localStorage.getItem(KEY));
-    if (localData !== null) {
-        emailInputEl.value = localData.email;
-        messageInputEl.value = localData.message;
+function resetForm() {
+    if (localStorage.getItem(KEY)) {
+        const localData = JSON.parse(localStorage.getItem(KEY));
+        console.log(Object.entries(localData).forEach(([key, value]) => {
+            feedBackForm.elements[key].value = value;
+        }));
     }
 };
 
-feedBackForm.addEventListener('input', throttle(hendlerSaveData, 500));
-feedBackForm.addEventListener('submit', hendlerSend);
+feedBackForm.addEventListener('input', throttle(hendlerInputForm, 1000));
+feedBackForm.addEventListener('submit', hendlerSavedata);
